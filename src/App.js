9e3cab4a6a4b17eef1d73db8cf162355b92b7ead
weilern17 <nick.weiler125@gmail.com>
2025-03-07 14:17:25 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState } from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+  const [file, setFile] = useState();
+
+    function handleChange(image) { //run when "select file" is clicked
+        
+
+      const selectedImage = image.target.files[0]; //array because could potentially select multiple files, not in this case.
+
+      if (!selectedImage) { //safely exits 
+        return;
+      }
+
+      const validImageTypes = ["image/png", "image/jpeg", "image/jpg", "image/gif", "image/webp"]; //allowed file types
+
+      if (validImageTypes.includes(selectedImage.type)) { //if right type, creates temporary to allow browser to display image
+        setFile(URL.createObjectURL(selectedImage));
+      } else {
+        setFile(null); // Reset file state to prevent errors
+      }
+      
+      //selectedImage is to be sent to back end
+
+    }
+
+    return (
+      <div className="App">
+      <h2>Add Image:</h2>
+      <div className="upload-container">
+        <label htmlFor="fileInput" className="upload-btn">Choose File</label>
+        <input 
+          id="fileInput" 
+          type="file" 
+          accept="image/png, image/jpeg, image/jpg, image/gif, image/webp" 
+          onChange={handleChange} 
+        />
+      </div>
+      {file && <img src={file} className="uploaded-image" alt="Uploaded preview" />}
     </div>
-  );
+    );
 }
 
 export default App;
